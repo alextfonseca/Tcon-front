db48e110a6056975c9bd6e2152daeb8831fac295
Alex Teixeira da Fonseca <alexatf2014@gmail.com>
2022-02-27 17:39:57 -0300

    fix font

--- App.tsx
+++ App.tsx
@@ -1,25 +1,15 @@
 import { useFonts } from "expo-font";
 
-import {
-  Nunito_700Bold,
-  Nunito_500Medium,
-  Nunito_400Regular,
-} from "@expo-google-fonts/nunito";
-
-import {
-  Raleway_300Light,
-  Raleway_400Regular,
-} from "@expo-google-fonts/raleway";
 import AppLoading from "expo-app-loading";
 import { Navigation } from "./src/components/Navigation";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Nunito_700Bold,
-    Nunito_500Medium,
-    Nunito_400Regular,
-    Raleway_300Light,
-    Raleway_400Regular,
+    "Nunito-Bold": require("./assets/fonts/Nunito-Bold.ttf"),
+    "Nunito-Medium": require("./assets/fonts/Nunito-Medium.ttf"),
+    "Nunito-Regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "Raleway-Regular": require("./assets/fonts/Raleway-Regular.ttf"),
+    "Raleway-Light": require("./assets/fonts/Raleway-Light.ttf"),
   });
 
   if (!fontsLoaded) {
